e71d3d95c292c537e26c2140701cb3b8874f44e0
Aleksandr Vasiljev <aleksvas2004@gmail.com>
2026-03-17 15:49:57 +0100

    merge conflict core/settings.py 'base',

--- core/settings.py
+++ core/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
     'base',
-=======
->>>>>>> feature/initial-setup
 ]
 
 MIDDLEWARE = [
